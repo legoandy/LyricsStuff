a1a1b473d8b79b815bcd10b15d5fc0b0b2fed91f
Andy <legoandy@gmail.com>
2015-11-11 22:01:03 -0800

    Add scrolling.

--- app/src/main/java/legoandy/com/lyricsstuff/MainActivity.java
+++ app/src/main/java/legoandy/com/lyricsstuff/MainActivity.java
@@ -1,10 +1,16 @@
 package legoandy.com.lyricsstuff;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
 import android.widget.TextView;
+
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,11 +20,16 @@ import java.io.Reader;
 public class MainActivity extends Activity {
 
     private static final String TAG = "LyricsMainActivity";
+    private GestureDetector mGestureDetector;
+    private ScrollView mScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGestureDetector = createGestureDetector(this);
+        mScrollView = (ScrollView) findViewById(R.id.scrollView);
 
         final TextView tv = (TextView) findViewById(R.id.textView);
         AsyncTask.execute(new Runnable() {
@@ -47,5 +58,54 @@ public class MainActivity extends Activity {
             s.append(buf, 0, n);
         }
         return s.toString();
+    }
+
+    private GestureDetector createGestureDetector(Context context) {
+        GestureDetector gestureDetector = new GestureDetector(context);
+        //Create a base listener for generic gestures
+        gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
+            @Override
+            public boolean onGesture(Gesture gesture) {
+                Log.e(TAG, "Gesture: " + gesture);
+                if (gesture == Gesture.TAP) {
+                    // do something on tap
+                    return true;
+                } else if (gesture == Gesture.TWO_TAP) {
+                    // do something on two finger tap
+                    return true;
+                } else if (gesture == Gesture.SWIPE_RIGHT) {
+                    // do something on right (forward) swipe
+                    return true;
+                } else if (gesture == Gesture.SWIPE_LEFT) {
+                    // do something on left (backwards) swipe
+                    return true;
+                }
+                return false;
+            }
+        });
+        gestureDetector.setFingerListener(new GestureDetector.FingerListener() {
+            @Override
+            public void onFingerCountChanged(int previousCount, int currentCount) {
+            }
+        });
+        gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
+            @Override
+            public boolean onScroll(float displacement, float delta, float velocity) {
+                mScrollView.smoothScrollBy(0, Math.round(delta));
+                return true;
+            }
+        });
+        return gestureDetector;
+    }
+
+    /*
+     * Send generic motion events to the gesture detector
+     */
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        if (mGestureDetector != null) {
+            return mGestureDetector.onMotionEvent(event);
+        }
+        return false;
     }
 }
